50aede763a680a548040a07dea57b0acc8408ce2
winvaz <winvaz@163.com>
2021-06-01 15:16:55 +0800

    Initial commit

---
+++ src/test/java/com/icore/winvaz/spring_cloud_git_config/SpringCloudGitConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icore.winvaz.spring_cloud_git_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGitConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
